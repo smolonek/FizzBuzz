c5ae36c72207322790f2c9a52c4d0407cb01f889
root <root@LAPTOP-II5AMA8R>
2019-03-11 19:00:05 +0100

    Add project files.

---
+++ WebApplication1/Controllers/FizzBuzzController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class FizzBuzzController : Controller
+    {
+        // GET: FizzBuzz
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Count()
+        {
+            Licz l = new Licz();
+            return View(l);
+        }
+        [HttpPost]
+        public ActionResult Count(Licz licz)
+        {
+            if (licz.liczba < 1 || licz.liczba > 100)
+            {
+                licz.wynik = "Nieprawidlowe dane!";
+                return View("out", licz);
+            }
+            if (licz.liczba %3 == 0 && licz.liczba % 5 == 0)
+            {
+                licz.wynik = "FizzBuzz";
+                return View("out", licz);
+            }
+            if (licz.liczba % 3 == 0)
+            {
+                licz.wynik = "Fizz";
+                return View("out", licz);
+            }
+            if (licz.liczba % 5 == 0)
+            {
+                licz.wynik = "Buzz";
+                return View("out", licz);
+            }
+            else
+            {
+                licz.wynik = licz.liczba.ToString("0");
+                return View("out", licz);
+            }
+        }
+    }
+}
---
+++ WebApplication1/Models/licz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace WebApplication1.Models
+{
+    public class Licz
+    {
+        [Display(Name = "Liczba")]
+        public int liczba { get; set; }
+        public string wynik;
+    }
+}
